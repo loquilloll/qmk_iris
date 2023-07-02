e5f66f6d226240df8e5f9c5b29f8309b5812a9c4
Alvin Sanchez <loquilloll@gmail.com>
2023-07-02 09:01:43 -0400

    IGNORE_MOD_TAP_INTERRUPT is default now

--- keymaps/loquillo/config.h
+++ keymaps/loquillo/config.h
@@ -19,7 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // #define USE_I2C
 #define EE_HANDS
-#define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_TERM_PER_KEY
 #define TAPPING_TERM 225
 #define TAPPING_TERM_SFT 175
